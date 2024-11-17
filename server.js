3ec3bdcaacb1382d7b3c97c94d1966bb2736625e
uwinezagloria <gloria7wineza.com>
2024-11-16 22:08:51 -0800

    swagger documentation

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ import cors from "cors"
 import errorHandler from "./middlewares/errorHandler.js";
 import customError from "./middlewares/customError.js";
 import router from "./router/index.js";
+import swagger from "./docs/swagger.json" assert{type:"json"}
+import swaggerUi from "swagger-ui-express"
 dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(router)
 app.use(cors())
+app.use("/API/ArtConnect",swaggerUi.serve, swaggerUi.setup(swagger))
 app.all('*',(req,res,next)=>{
     const err=new customError(`Can't find ${req.originalUrl} on this Server`,404)
     next(err)
